636e6c463e1c354b54b01de22345d3393b47b841
Iulia Constantin <33063360+iulia2799@users.noreply.github.com>
2024-06-04 15:44:17 +0300

    todate()

--- functions/src/helpers.ts
+++ functions/src/helpers.ts
@@ -1,10 +1,10 @@
 import { Timestamp } from "firebase-admin/firestore";
 
 export function convertTimestampToDate(timestamp: Timestamp) {
-  const date = new Date(
-    timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
-  );
-  return date.toString();
+  //const date = new Date(
+  //  timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+  //);
+  return timestamp.toDate();
 }
 
 export function getDailySeconds(date: number) {
